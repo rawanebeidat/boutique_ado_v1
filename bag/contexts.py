12a860a1616f771c65151618407b8b4ddfffdeb6
rawanebeidat <r.ebeidat@hotmail.com>
2021-03-29 01:09:31 +0000

    added quantity boxes to shopping bag

--- bag/contexts.py
+++ bag/contexts.py
@@ -3,7 +3,6 @@ from django.conf import settings
 from django.shortcuts import get_object_or_404
 from products.models import Product
 
-
 def bag_contents(request):
 
     bag_items = []
@@ -28,7 +27,7 @@ def bag_contents(request):
                 product_count += quantity
                 bag_items.append({
                     'item_id': item_id,
-                    'quantity': item_data,
+                    'quantity': quantity,
                     'product': product,
                     'size': size,
                 })
@@ -39,7 +38,9 @@ def bag_contents(request):
     else:
         delivery = 0
         free_delivery_delta = 0
+    
     grand_total = delivery + total
+    
     context = {
         'bag_items': bag_items,
         'total': total,
