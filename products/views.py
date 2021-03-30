bad28886a87df159236ec3d2befb504bd94ffe96
rawanebeidat <r.ebeidat@hotmail.com>
2021-03-30 00:00:15 +0000

    added notification

--- products/views.py
+++ products/views.py
@@ -3,11 +3,9 @@ from django.contrib import messages
 from django.db.models import Q
 from django.db.models.functions import Lower
 
-
 from .models import Product, Category
 
 # Create your views here.
-
 
 def all_products(request):
     """ A view to show all products, including sorting and search queries """
